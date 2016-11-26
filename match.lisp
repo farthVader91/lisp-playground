95095c04a312ddecba37949d22a67c9df0e5b18d
Vishal Gowda <cartmanboy1991@gmail.com>
2016-11-26 15:22:14 +0530

    Define function "dont-care"
    Re-define function "match-element" to use function "dont-care"

--- match.lisp
+++ match.lisp
@@ -1,6 +1,6 @@
 (defpackage match
  (:use "COMMON-LISP" "COMMON-LISP-USER")
- (:export :variablep :match-element)
+ (:export :variablep :match-element :dont-care)
 )
 (in-package "MATCH")
 
@@ -9,5 +9,15 @@
 )
 
 (defun match-element (e1 e2)
- (or (eql e1 e2) (or (variablep e1) (variablep e2)))
+ (cond ((and (not (dont-care e1)) (variablep e1))
+            (list e1 e2))
+       ((and (not (dont-care e2)) (variablep e2))
+            (list e2 e1))
+       (t
+            (or (eql e1 e2) (or (dont-care e1) (dont-care e2))))
+ )
+)
+
+(defun dont-care (e)
+    (if (symbolp e) (string= (symbol-name e) #\?) nil)
 )
