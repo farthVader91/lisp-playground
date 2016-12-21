c2d7ffdd6b66a435e0e812dec48ca0c24cdf31b0
farthVader91 <cartmanboy1991@gmail.com>
2016-12-21 18:06:32 +0530

    Add snippets for ch: recursion on lists P2

--- set.lisp
+++ set.lisp
@@ -4,8 +4,8 @@
 
 (defpackage set
 	(:use :cl :util)
-	(:export setp set makeset union)
-	(:shadow set union))
+	(:export setp set makeset union first rest insert intersection complement subsetp)
+	(:shadow set union first rest insert intersection complement subsetp))
 
 (in-package :set)
 
@@ -13,9 +13,8 @@
 	"Return True if L is a list of distinct objects,
          False otherwise."
 	(cond ((not (listp l)) nil)
-              ((null l) T)
-	      ((member (first l) (rest l)) nil)
-	      (t (setp (rest l)))))
+	      ((string= (cl:first l) :set))))
+
 
 (deftype set ()
          "Set object that satisfies setp"
@@ -23,9 +22,9 @@
 
 (defun unlabelled-makeset (b)
 	(cond ((null b) '())
-	      ((member (first b) (rest b))
-	       (unlabelled-makeset (rest b)))
-	      (t (cons (first b) (unlabelled-makeset (rest b))))))
+	      ((member (cl:first b) (cl:rest b))
+	       (unlabelled-makeset (cl:rest b)))
+	      (t (cons (cl:first b) (unlabelled-makeset (cl:rest b))))))
 
 (defun makeset (b)
         "Return an unordered collection of unique elements,
@@ -33,11 +32,75 @@
         (check-type b util:bag)
 	(cons :set (unlabelled-makeset b)))
 
+(defun union-unlabelled-sets (s1 s2)
+	(cond ((null s1) s2)
+	      ((member (cl:first s1) s2)
+	       (union-unlabelled-sets (cl:rest s1) s2))
+	      (t (cons (cl:first s1) (union-unlabelled-sets (cl:rest s1) s2)))))
+
 (defun union (s1 s2)
         "Return the union of 2 sets s1 and s2."
 	(check-type s1 set)
 	(check-type s2 set)
-	(cond ((null s1) s2)
-	      ((member (first s1) s2)
-	       (union (rest s1) s2))
-	      (t (cons (first s1) (union (rest s1) s2)))))
+	(union-unlabelled-sets (cl:rest s1) (cl:rest s2)))
+
+(defun first (s)
+        "Return the first element listed in a set S."
+	(check-type s set)
+	(cl:first (cl:rest s)))
+
+(defun rest (s)
+        "Return the rest of the set S without the first element."
+	(check-type s set)
+	(cl:rest (cl:rest s)))
+
+(defun insert (e s)
+        "Insert an element E into S if it already doesn't exist in S.
+         Finally return S."
+	(check-type s set)
+        (if (member e s) s
+	    (cons e s)))
+
+(defun empty (s)
+        "Return True if set S is empty, False otherwise."
+	(check-type s set)
+	(null (rest s)))
+
+(defun intersection-unlabelled (s1 s2)
+	(cond ((null s1) '())
+	      ((member (cl:first s1) s2)
+	       (cons
+		(cl:first s1)
+		(intersection-unlabelled (cl:rest s1) s2)))
+	      (t (intersection-unlabelled (cl:rest s1) s2))))
+
+(defun intersection (s1 s2)
+        "Return set of elements that are common across sets S1 and S2."
+	(check-type s1 set)
+	(check-type s2 set)
+	(intersection-unlabelled (cl:rest s1) (cl:rest s2)))
+
+(defun complement-unlabelled (s1 s2)
+        (cond ((null s1) '())
+	      ((member (cl:first s1) s2)
+	       (complement-unlabelled (cl:rest s1) s2))
+	      (t (cons (cl:first s1)
+		       (complement-unlabelled (cl:rest s1) s2)))))
+
+(defun complement (s1 s2)
+        "Return set of disjoint elements across sets S1 and S2."
+	(check-type s1 set)
+	(check-type s2 set)
+	(complement-unlabelled (cl:rest s1) (cl:rest s2)))
+
+(defun subsetp-unlabelled (s1 s2)
+	(cond ((null s1) t)
+	      ((member (cl:first s1) s2) (subsetp-unlabelled (cl:rest s1) s2))
+	      (t nil)))
+
+(defun subsetp (s1 s2)
+        "Return True if all elements of set S1 are members of set S2,
+         False otherwise."
+	(check-type s1 set)
+	(check-type s2 set)
+	(subsetp-unlabelled (cl:rest s1) (cl:rest s2)))
