7071c4973e3bf993263164859fc4cd3aab440faa
farthVader91 <cartmanboy1991@gmail.com>
2016-12-29 18:24:51 +0530

    some stuff

--- calculator.lisp
+++ calculator.lisp
@@ -2,8 +2,19 @@
  (:use "COMMON-LISP")
  (:export "COMBINE-EXPR")
 )
+
 (in-package "CALCULATOR")
+
 (defun combine-expr (operator operand expr)
  (cons (list operator operand (car expr)) (cdr expr))
 )
-(export 'combine-expr)
+
+(defun enclose-expression1 (expr last)
+  "Convert an expression in in-fix notation to Cambridge-Prefix
+   notation."
+  (check-type expr list)
+  (cond ((null expr) 
+    (or
+       (string= (symbol-name (first expr) "+"))
+       (string= (symbol-name (first expr) "-"))
+      )
